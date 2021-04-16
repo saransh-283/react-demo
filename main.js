0c05e520952c2a9fc2fff2d765f38214b1bb9ccb
saransh-283 <saranshmm@gmail.com>
2021-04-16 23:10:13 +0530

    Bulb Complete

--- main.js
+++ main.js
@@ -32,5 +32,62 @@ class Counter extends React.Component {
         </div>
     }
 }
-//qwertyyui
+
 ReactDOM.render(<Counter />, document.getElementById('counter'))
+
+// Bulb
+
+class Bulb extends React.Component {
+    constructor(props) {
+        super(props)
+        this.something=this.something.bind(this)
+    }
+
+    something(){
+        console.log(this.props.onOrOff)
+    }
+    render() {
+        return <div>
+            <div onClick={this.something} id="bulb-circle" className={"bulb-"+this.props.onOrOff} style={{ background: "rgb("+(255-(15*(10-this.props.brightness)))+','+(255-(15*(10-this.props.brightness)))+',0)' }}></div>
+        </div>
+    }
+}
+
+class BulbContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            brightness: 10,
+            on: false
+        }
+        this.brightnessChange = this.brightnessChange.bind(this)
+        this.onOffBtn = this.onOffBtn.bind(this)
+    }
+
+    brightnessChange(event) {
+        this.setState({
+            brightness: event.target.value
+        })
+    }
+
+    onOffBtn() {
+        this.setState(function (state) {
+            return {
+                on: !state.on,
+            }
+        })
+        console.log(this.state.on)
+        console.log(this.state.brightness)
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.onOffBtn}>On/Off</button>
+            <input disabled={!this.state.on} onChange={this.brightnessChange} type="range" name="brightness" min="1" max="10" value={this.state.brightness} />
+            <Bulb onOrOff={this.state.on} brightness={this.state.brightness} />
+        </div>
+    }
+
+}
+
+ReactDOM.render(<BulbContainer />, document.getElementById('bulb'))
